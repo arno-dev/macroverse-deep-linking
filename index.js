22868284eca288ca7d13113497ce9d93d2b27fe5
Arnaud <arnaud.phommasone@gmail.com>
2020-12-05 21:14:26 +0700

    Update the assetlinkins file

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const port = 5000
 // below rule for  '../react-ui/build' ...
 
 var appleAppSiteAssociationContents = fs.readFileSync(path.resolve(__dirname, './assets/apple-app-site-association'), 'utf8');
+var assetLinksContents = fs.readFileSync(path.resolve(__dirname, './assets/assetlinks.json'), 'utf8');
 
-// Host apple-app-site-association file for Apple iOS Associated Domains https://developer.apple.com/library/archive/documentation/General/Conceptual/AppSearch/UniversalLinks.html#//apple_ref/doc/uid/TP40016308-CH12-SW1
+// Host apple - app - site - association file for Apple iOS Associated Domains https://developer.apple.com/library/archive/documentation/General/Conceptual/AppSearch/UniversalLinks.html#//apple_ref/doc/uid/TP40016308-CH12-SW1
 app.get(['/apple-app-site-association', '/.well-known/apple-app-site-association'], function (req, res) {
     res.set('Content-Type', 'application/json');
     res.send(appleAppSiteAssociationContents);
+});
+
+app.get(['/assetlinks.json', '/.well-known/assetlinks.json'], function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send(assetLinksContents);
 });
 
 app.get('/', function (req, res) {
